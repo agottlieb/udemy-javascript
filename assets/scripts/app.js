ba39b8cfd03ad4c9c6fe135265bb0b53612cae8a
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-19 13:15:09 -0400

    start of functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2)  {
+    const result = num1 + num2; 
+    alert('The Result is' + result);
+}
+
+add(4,7);
+add(5,7);
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
