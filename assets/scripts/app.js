ba113415a47a68d85e482624d46dee1f04a92e22
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-19 14:43:41 -0400

    stopped mid-module

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add()  {
+    const calcDescription = `${currentResult} +${userInput.value}`
     currentResult= currentResult + parseInt(userInput.value);
-    outputResult(currentResult, ''); 
+    outputResult(currentResult, calcDescription); 
 }
 
 addBtn.addEventListener('click', add);
