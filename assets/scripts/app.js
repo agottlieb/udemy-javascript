fdc765613e12e28d585eaa6068df5a384f104877
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-20 10:54:14 -0400

    seperated function logic to reuse

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,23 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserInput () {
+    return parseInt(userInput.value);
+}
+
 function add()  {
-    const calcDescription = `${currentResult} +${userInput.value}`
-    currentResult= currentResult + parseInt(userInput.value);
+    const enteredNumber = getUserInput;
+    const calcDescription = `${currentResult} +${enteredNumber}`
+    currentResult= currentResult + enteredNumber;
     outputResult(currentResult, calcDescription); 
+}
+
+function subtract () {
+    const enteredNumber = getUserInput;
+    const calcDescription = `${currentResult} - ${enteredNumber}`
 }
 
 addBtn.addEventListener('click', add);
 
-//let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
 
