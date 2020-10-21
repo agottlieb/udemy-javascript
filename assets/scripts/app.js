a36f8fdf97c2de5f83aff379df6671fc4451abe1
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-21 14:34:06 -0400

    added if statements for multiply and divide

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -34,9 +34,15 @@ function calculateResult(calculationType) {
   if (calculationType ==='ADD') {
     currentResult += enteredNumber;
     mathOperator ='+';
-  } else {
+  } else if (calculationType ==='SUBTRACT'){
     currentResult -= enteredNumber; 
     mathOperator ='-';
+  } else if (calculationType==='MULTIPLY'){
+    currentResult *= enteredNumber;
+    mathOperator ='*';
+  } else if (calculationType==='DIVIDE'){
+    currentResult /= enteredNumber;
+    mathOperator ='/';
   }
   createAndWriteOutput(mathOperator, initialResult, enteredNumber);
   writeToLog(calculationType, initialResult, enteredNumber, currentResult);
@@ -51,19 +57,11 @@ function subtract() {
 }
 
 function multiply() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult *= enteredNumber;
-  createAndWriteOutput('*', initialResult, enteredNumber);
-  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+  calculateResult('MULTIPLY');
 }
 
 function divide() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult /= enteredNumber;
-  createAndWriteOutput('/', initialResult, enteredNumber);
-  writeToLog('DIVIDE', initialResult, enteredNumber, currentResult)
+  calculateResult('DIVIDE');
 }
 
 addBtn.addEventListener('click', add);
