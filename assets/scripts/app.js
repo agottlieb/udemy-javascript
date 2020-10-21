4545063277f72a8536e9423a8c843a4c0494230f
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-21 14:26:05 -0400

    added loops to add and subtract

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -27,20 +27,27 @@ function writeToLog (
         console.log(logEntries);
 }
 
-function add() {
+function calculateResult(calculationType) {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult += enteredNumber;
-  createAndWriteOutput('+', initialResult, enteredNumber);
-  writeToLog('ADD', initialResult, enteredNumber, currentResult);
+  let mathOperator; 
+  if (calculationType ==='ADD') {
+    currentResult += enteredNumber;
+    mathOperator ='+';
+  } else {
+    currentResult -= enteredNumber; 
+    mathOperator ='-';
+  }
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
+
+function add() {
+  calculateResult('ADD');
 }
 
 function subtract() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult -= enteredNumber;
-  createAndWriteOutput('-', initialResult, enteredNumber);
-  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+  calculateResult('SUBTRACT');
 }
 
 function multiply() {
