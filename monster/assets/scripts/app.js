2a9c5e94b134a299d53cffde21b302f81ea2f857
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-22 11:00:32 -0400

    added if statements to check for wins and losses

--- monster/assets/scripts/app.js
+++ monster/assets/scripts/app.js
@@ -1,5 +1,6 @@
 //hard-coded value in all caps
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE=14;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
@@ -10,6 +11,14 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler () {
    const damage= dealMonsterDamage(ATTACK_VALUE);
    currentMonsterHealth -=damage; //-= means subtracts damage and assigns the outcome as the new value of current monster health
+   const playerdamaged=dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -=playerdamaged;
+   if (currentMonsterHealth <= 0 && currentPlayerHealth >0) {
+        alert('You win!');
+    } else if (currentPlayerHealth <=0 && currentMonsterHealth >0){
+        alert ('You lost!');
+    } else if (currentPlayerHealth <=0 && currentMonsterHealth <=0)
+        alert ('You have a draw')
 }
 
 attackBtn.addEventListener('click', attackHandler)
