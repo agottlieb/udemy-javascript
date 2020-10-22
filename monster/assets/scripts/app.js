3ace80d32569bb0e74ba1da4f835cd67341b55e5
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-22 10:27:03 -0400

    added function for attack monster

--- monster/assets/scripts/app.js
+++ monster/assets/scripts/app.js
@@ -1,2 +1,15 @@
+//hard-coded value in all caps
 const ATTACK_VALUE = 10;
 let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+//function is called by render.js, sets the players' maximum life in the bars
+adjustHealthBars(chosenMaxLife); 
+
+function attackHandler () {
+   const damage= dealMonsterDamage(ATTACK_VALUE);
+   currentMonsterHealth -=damage; //-= means subtracts damage and assigns the outcome as the new value of current monster health
+}
+
+attackBtn.addEventListener('click', attackHandler)
