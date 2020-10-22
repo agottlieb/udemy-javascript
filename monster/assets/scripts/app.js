1392ec0386c6fb6e68cc85fc359ce5b681bf34bb
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-22 11:28:07 -0400

    refactored code to put logic into one new function and have lean handlers

--- monster/assets/scripts/app.js
+++ monster/assets/scripts/app.js
@@ -1,6 +1,7 @@
 //hard-coded value in all caps
 const ATTACK_VALUE = 10;
-const MONSTER_ATTACK_VALUE=14;
+const MONSTER_ATTACK_VALUE= 14;
+const STRONG_ATTACK_VALUE= 17;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
@@ -8,17 +9,32 @@ let currentPlayerHealth = chosenMaxLife;
 //function is called by render.js, sets the players' maximum life in the bars
 adjustHealthBars(chosenMaxLife); 
 
+function attackMonster (mode) {
+    let maxDamage;
+    if (mode ==='ATTACK') {
+        maxDamage=ATTACK_VALUE;
+    } else if (mode ==='STRONG_ATTACK'){
+        maxDamage=STRONG_ATTACK_VALUE;
+    }
+    const damage= dealMonsterDamage(maxDamage);
+    currentMonsterHealth -=damage; //-= means subtracts damage and assigns the outcome as the new value of current monster health
+    const playerdamaged=dealPlayerDamage(MONSTER_ATTACK_VALUE);
+     currentPlayerHealth -=playerdamaged;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth >0) {
+         alert('You win!');
+     } else if (currentPlayerHealth <=0 && currentMonsterHealth >0){
+         alert ('You lost!');
+     } else if (currentPlayerHealth <=0 && currentMonsterHealth <=0)
+         alert ('You have a draw')
+}
+
 function attackHandler () {
-   const damage= dealMonsterDamage(ATTACK_VALUE);
-   currentMonsterHealth -=damage; //-= means subtracts damage and assigns the outcome as the new value of current monster health
-   const playerdamaged=dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -=playerdamaged;
-   if (currentMonsterHealth <= 0 && currentPlayerHealth >0) {
-        alert('You win!');
-    } else if (currentPlayerHealth <=0 && currentMonsterHealth >0){
-        alert ('You lost!');
-    } else if (currentPlayerHealth <=0 && currentMonsterHealth <=0)
-        alert ('You have a draw')
+    attackMonster('ATTACK')
+}
+
+function strongAttackHandler () {
+    attackMonster('STRONG_ATTACK')
 }
 
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', strongAttackHandler )
