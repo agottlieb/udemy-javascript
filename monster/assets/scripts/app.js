7b69b1981652c7d0e28e81abc468737e7a2abdfe
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-23 11:11:12 -0400

    added ternery operator

--- monster/assets/scripts/app.js
+++ monster/assets/scripts/app.js
@@ -127,15 +127,15 @@ function endRound () {
 }
 //parameter mode identifies and designates attack type, used to be a string now its a global constant
 function attackMonster(mode) {
-    let maxDamage;
-    let logEvent;
-    if (mode === MODE_ATTACK) {
-      maxDamage = ATTACK_VALUE;
-      logEvent = LOG_EVENT_PLAYER_ATTACK;
-    } else if (mode === MODE_STRONG_ATTACK) {
-      maxDamage = STRONG_ATTACK_VALUE;
-      logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
-    }
+    const maxDamage = mode ===MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+    const logEvent = mode ===MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK ;
+    // if (mode === MODE_ATTACK) {
+    //   maxDamage = ATTACK_VALUE;
+    //   logEvent = LOG_EVENT_PLAYER_ATTACK;
+    // } else if (mode === MODE_STRONG_ATTACK) {
+    //   maxDamage = STRONG_ATTACK_VALUE;
+    //   logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
+    // }
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage; //-= means subtracts damage and assigns the outcome as the new value of current monster health
     writeToLog(
