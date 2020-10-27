96e7a98160675eb2c876c738c44faef56a2be3c2
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-27 12:33:12 -0400

    adding game logic

--- game/app.js
+++ game/app.js
@@ -5,6 +5,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = 'ROCK';
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS'; 
 
 let gameIsRunning =false; 
 
@@ -14,14 +17,32 @@ const getPlayerChoice = function () {
         selection !== PAPER &&
         selection !== SCISSORS
     ) {
-        alert (`Invalid choice; ${DEFAULT_USER_CHOICE}is default`);
+        alert (`Invalid choice; ${DEFAULT_USER_CHOICE} is default`);
         return DEFAULT_USER_CHOICE;
     }
     return selection;
 };
 
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK
+    } else if 
+        (randomValue < 0.67) {
+        return PAPER
+        }
+    else {return SCISSORS}
+};
 
-
+const getWinner = function (cChoice, pChoice ) {
+    if (cChoice ===pChoice) {
+        return RESULT_DRAW
+    } else if (cChoice ===ROCK && pChoice ===PAPER || 
+        cChoice ===SCISSORS && pChoice === ROCK ||
+        cChoice===PAPER && pChoice === SCISSORS)
+    { return RESULT_PLAYER_WINS}
+    else { return RESULT_COMPUTER_WINS};
+} 
 
 startGameBtn.addEventListener('click', function () {
     if (gameIsRunning) { 
@@ -30,7 +51,10 @@ startGameBtn.addEventListener('click', function () {
     gameIsRunning = true;
     console.log('Game is starting...'); 
     const playerChoice= getPlayerChoice();
-    console.log(playerChoice);
+    const computerChoice = getPlayerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+    console.log(winner);
+ 
 }); 
 
 
