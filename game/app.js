c58f5c7614e24bdc0365ac73d06aa8010cd70e26
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-27 10:45:46 -0400

    added anonymous function

--- game/app.js
+++ game/app.js
@@ -1,12 +1,17 @@
 //getElementbyId is a method--function as an object--startGameBtn has stored object
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame () {
-    console.log('Game is starting...');
-}
+//function as an expression + anonymous function
+// const start = function() { //storing a function in a variable, call the const to call the function
+//     console.log('Game is starting...');
+//};
 
 //startGame(); example of direct execution 
 
 //console.dir(startGame); functions are also objects
 
-startGameBtn.addEventListener('click', startGame); //addEventListener is a method- stored in an ojbect
+//addEventListener is a method- stored in an ojbect
+//putting in start as anonymous function bc only used once
+startGameBtn.addEventListener('click', function () {
+    console.log('Game is starting...');
+}); 
