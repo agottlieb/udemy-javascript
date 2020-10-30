2ba6aef0460483c965c8e7caf25d14348a5ecc0b
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-29 20:33:57 -0400

    added backdrop

--- dom/assets/scripts/app.js
+++ dom/assets/scripts/app.js
@@ -1,10 +1,16 @@
 const addMovieModal =  document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
+const addBackdrop = document.getElementById('backdrop');
 
 //classlist is read-only, but can toggle/remove and add css properties
 //keeps the other classes there 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible'); 
+    toggleBackdrop();
+};
+
+const toggleBackdrop = () => {
+    addBackdrop.classList.toggle('visible')
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
