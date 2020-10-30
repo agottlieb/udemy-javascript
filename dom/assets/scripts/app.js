39c5b29aa5f5efb2e2343fe35147d9eba428f972
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-10-29 21:08:14 -0400

    added input functionality

--- dom/assets/scripts/app.js
+++ dom/assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModal =  document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const addBackdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
 
 //classlist is read-only, but can toggle/remove and add css properties
 //keeps the other classes there 
@@ -22,6 +24,20 @@ const cancelButtonHandler = () => {
     toggleMovieModal();
 }
 
+const confirmMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if (titleValue.trim() === '' || imageUrlValue.trim() === '' || ratingValue.trim() === '') 
+    {
+        alert ('Please enter valid values')
+        return;
+    }
+
+}
+
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 addBackdrop.addEventListener('click', toggleBackdropHandler);
 cancelAddMovieButton.addEventListener('click', cancelButtonHandler);
+confirmAddddMovieButton.addEventListener('click', confirmMovieHandler);
