27f15564e0578eda6467a8596048ee085926bf1f
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-11-05 10:40:57 -0500

    started class structure for OOP

--- OOP/assets/scripts/app.js
+++ OOP/assets/scripts/app.js
@@ -1,12 +1,19 @@
+class Product {
+    title= 'DEFAULT';
+    imageUrl;
+    description;
+    price;
+}
+
 const productList = {
     products: [
         {title: 'Narwhal Pillow', 
-        imageURL:'https://res-1.cloudinary.com/mp-assets/image/upload/c_pad,f_auto,q_auto,w_550/v1515689100/i-scream/content/images/0/0002723_blue-narwhal-scented-microbead-pillow.png' ,
+    imageURL:'https://res-1.cloudinary.com/mp-assets/image/upload/c_pad,f_auto,q_auto,w_550/v1515689100/i-scream/content/images/0/0002723_blue-narwhal-scented-microbead-pillow.png' ,
         price: 20, 
         description: 'Unicorn of the Sea'
         } ,
         {title: 'Unicorn Floater', 
-        imageURL:'https://assets.ptimgs.com/ptimgs/rk/images/dp/wcm/202026/0013/img1c.jpg',
+        imageUrl:'https://assets.ptimgs.com/ptimgs/rk/images/dp/wcm/202026/0013/img1c.jpg',
         price: 25, 
         description: 'Unicorn Floater'
         }
@@ -22,7 +29,7 @@ render() {
         productElement.className = 'product-item';
         productElement.innerHTML = `
             <div>
-            <img src = "${prod.imageURL}" alt = "${prod.title}">
+            <img src = "${prod.imageUrl}" alt = "${prod.title}">
             <div class="product-item__content"> 
             <h2> ${prod.title}</h2>
             <h3>\$${prod.price}</h3>
