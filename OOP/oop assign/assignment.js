17252ed31abbd2967a3f60d2965204350de4e149
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-11-11 20:54:13 -0500

    section 1 with edits

--- OOP/oop assign/assignment.js
+++ OOP/oop assign/assignment.js
@@ -1,15 +1,19 @@
 class Course {
-    constructor (title, length, price) {
-    this.title =title;
-    this.length = length;
-    this.price= price;}
+    constructor (courseTitle, courseLength, coursePrice) {
+    this.title =courseTitle;
+    this.length = courseLength;
+    this.price= coursePrice;}
 }
 
-class CourseList {
-    courses = [
-        new Course ('Shakespeare', '5 hours', 40),
-        new Course ('Western Medicine', '7 hours', 50)
-    ]
-}
+// class CourseList {
+//     courses = [
+//         new Course ('Shakespeare', '5 hours', 40),
+//         new Course ('Western Medicine', '7 hours', 50)
+//     ]
+// }
 
-console.log (CourseList);
+const litCourse = new Course('Shakespeare', 5, 40);
+const medCourse = new Course('Western Medicine', 6, 50);
+
+console.log (litCourse);
+console.log (medCourse);
