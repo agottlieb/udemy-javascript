ef29e2c09db367f5a122801b8a616d65081fc881
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-11-11 21:28:18 -0500

    edits on sectin 3

--- OOP/oop assign/assignment.js
+++ OOP/oop assign/assignment.js
@@ -13,11 +13,33 @@ class Course {
     }
 }
 
+class PracticalCourse extends Course {
+    constructor (title, length, price, exercisesCount) {
+        super(title, length, price); //super executes the arguments from the parent class constructor
+        this.numberOfExercises = exercisesCount; //new function
+    }
+}
+
+class TheoryCourse extends Course {
+    //don't need to add constructor if keeping all the properties
+    // constructor (title, length, price) {
+    //     super(title, length, price);
+    // }
+    publish () {
+        console.log('Publishing')
+    }
+}
+
 const litCourse = new Course('Shakespeare', 5, 40);
 const medCourse = new Course('Western Medicine', 6, 50);
+const mathCourse = new PracticalCourse ('Math for Dummies', 8, 70, 100);
+const philCourse = new TheoryCourse ('Ethics', 3, 15);
 
 console.log (litCourse);
 console.log (medCourse);
+console.log (mathCourse);
+console.log (philCourse);
 
 console.log(litCourse.value());
 console.log(litCourse.summary())
+console.log (philCourse.publish());
