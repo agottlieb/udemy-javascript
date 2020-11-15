240fe9151351718280f410e149c77c430639981a
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2020-11-14 21:26:31 -0500

    some changes to moving items

--- OOP/oop-practice/assets/scripts/app.js
+++ OOP/oop-practice/assets/scripts/app.js
@@ -3,16 +3,40 @@ class Tooltip {
 }
 
 class ProjectItem {
-    constructor(task, description)
-    finish () {    
+    constructor(id){
+        this.id = id;   
+        this.connectSwitchButton(); 
+        this.connectMoreInfoButton();
+    }
+
+    connectMoreInfoButton() {
+    }
+    connectSwitchButton () {
+        const projectItemElement = document.getElementById(this.id);
+        const switchBtn = projectItemElement.querySelector('button:last-of-type');
+        switchBtn.addEventListener('click', this.updateProjectListsHandler);
     }
 }
 
 //putting in the logic to parse out which items go into which list
 class ProjectList {
- constructor (type) {
-    const projecItems = document.querySelectorAll(`#${type}-projects li`);
+    projects = []; //gets converted into properties 
+     
+  constructor (type) { //constructor differentiates by type
+    const projectItems = document.querySelectorAll(`#${type}-projects li`); //CSS selector
+    for (const projectItem of projectItems) {
+        this.projects.push(
+            new ProjectItem(projectItem.id)); //accesses id attribute, JS representation of the id attribute
+    }
+    console.log(this.projects);
  }
+
+    addProject() {}
+
+    switchProject(projectId) {
+        this.switchHandler(this.projects.find(p => p.id === projectId));
+        this.projects = this.projects.filter(p => p.id !== projectId);
+    }
 }
 
 class App {
